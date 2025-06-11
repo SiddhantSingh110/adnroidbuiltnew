fbfe7abd98936380d60494d3512bcb915aa52f4a
siddsingh110 <siddsingh110@gmail.com>
2025-06-11 18:02:37 +0530

    Fix context creation to use EXPO_ROUTER_APP_ROOT env var

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ import { ExpoRoot } from 'expo-router';
 
 // Must be exported or Fast Refresh won't update the context
 export function App() {
-  const ctx = require.context('./app');
+  const ctx = require.context(
+    process.env.EXPO_ROUTER_APP_ROOT,
+    true,
+    /^(?:\.\/)(?!(?:(?:(?:.*\+api)|(?:\+html)))\.[tj]sx?$).*\.[tj]sx?$/
+  );
   return <ExpoRoot context={ctx} />;
 }
 
