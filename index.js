945ecd03362b33bc7660558a6bd07227ceb4a750
siddsingh110 <siddsingh110@gmail.com>
2025-06-11 14:42:41 +0530

    Add custom entry point to fix ContextNavigator production build

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js (in project root)
+import { registerRootComponent } from 'expo';
+import { ExpoRoot } from 'expo-router';
+
+// Must be exported or Fast Refresh won't update the context
+export function App() {
+  const ctx = require.context('./app');
+  return <ExpoRoot context={ctx} />;
+}
+
+registerRootComponent(App);
